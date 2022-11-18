1f81ed169c79bf03c1a21fd3811fb53d55a814a2
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-18 15:42:56 +0530

    Added: Add element to linked list at tail

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,9 +3,11 @@ package com.bridgelab.linkedlist;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<>();
-		list.add(70);
-		list.add(30);
-		list.add(56);
+		
+		list.append(56);
+		list.append(30);
+		list.append(70);
 		list.display();
-	}	
+		
+	}
 }
